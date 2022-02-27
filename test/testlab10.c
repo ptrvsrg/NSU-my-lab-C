d561eed2bca6d0e3fc1213d17a7659b9c2472a3f
ptrvsrg <s.petrov1@g.nsu.ru>
2022-02-27 23:19:21 +0700

    Fix test array and CheckFromArray

--- test/testlab10.c
+++ test/testlab10.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_POINT_COUNT 100000
+
 typedef struct 
 {
     int x, y;
@@ -14,41 +15,33 @@ static const struct { const char *const in, *const outError; int pointCount; TPo
     { "4\n2 9\n2 1\n2 -6\n2 4\n", NULL, 2, { {2, -6}, {2, 9} } }, // 2
     { "100001\n", "bad number of points", 0, { {0, 0} } }, // 3
     { "5\n1 1\n4 5\n", "bad number of lines", 0, { {0, 0} } }, // 4
-
-    { "0\n", NULL, 0, { {0, 0} } }, // 5
-    { "4\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n", NULL, 4, { {0, INT_MIN}, {INT_MAX, 0}, {0, INT_MAX}, {INT_MIN, 0} } }, // 6
-    { "5\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n0 0\n", NULL, 4, { {0, INT_MIN}, {INT_MAX, 0}, {0, INT_MAX}, {INT_MIN, 0} } }, // 7
-    { "8\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n-2147483648 -2147483648\n-2147483648 2147483647\n2147483647 -2147483648\n2147483647 2147483647\n", NULL, 4, { {INT_MIN, INT_MIN}, {INT_MAX, INT_MIN}, {INT_MAX, INT_MAX}, {INT_MIN, INT_MAX} } }, // 8
-    
-    { "4\n4 4\n2 3\n4 0\n0 4\n", NULL, 3, { {4, 4}, {4, 0}, {0, 4} } }, // 9
-    { "1\n2 3\n", NULL, 1, { {2, 3} } }, // 10
-    { "2\n1 1\n4 5\n", NULL, 2, { {1, 1}, {4, 5} } }, // 11
-    { "5\n0 0\n2 3\n4 0\n0 4\n4 4\n", NULL, 4, { {0, 0}, {4, 0}, {0, 4}, {4, 4} } }, // 12
-
-    { "-1\n", "bad number of points", 0, { {0, 0} } }, // 13
-    { "3\n1 2\n4 3\n5\n", "bad number of lines", 0, { {0, 0} } }, // 14
-    { "3\n4 0\n0 0\n0 4\n", NULL, 3, { {0, 0}, {0, 4}, {4, 0} } }, // 15
-    { "3\n-7 3\n0 0\n7 -3\n", NULL, 2, { {-7, 3}, {7, -3} } }, // 16
-
-    { "8\n2 4\n-2 4\n0 0\n-3 9\n1 1\n-1 1\n3 9\n0 -6\n", NULL, 3, { {3, 9}, {-3, 9}, {0, -6} } }, // 17
-    { "8\n1 -1\n-1 -1\n2 -4\n-3 -9\n-2 -4\n0 0\n3 -9\n0 6\n", NULL, 3, { {3, -9}, {-3, -9}, {0, 6} } }, // 18
-    { "6\n-4 -1\n2 2\n1 4\n-2 -2\n-1 -4\n4 1\n", NULL, 4, { {4, 1}, {1, 4}, {-4, -1}, {-1, -4} } }, // 19
-    { "6\n3 3\n1 4\n-1 -4\n-4 -1\n4 1\n-3 -3\n", NULL, 6, { {4, 1}, {1, 4}, {-4, -1}, {-1, -4}, {3, 3}, {-3, -3} } }, // 20
-    
-    { "8\n7 0\n4 0\n0 0\n3 0\n2 0\n1 0\n5 0\n6 0\n", NULL, 2, { {0, 0}, { 7, 0 } } }, // 21
-    { "8\n0 7\n0 4\n0 0\n0 3\n0 2\n0 1\n0 5\n0 6\n", NULL, 2, { {0, 0}, {0, 7} } }, // 22
-    { "8\n3 3\n-1 -1\n5 5\n-4 -4\n1 1\n2 2\n-2 -2\n3 3\n", NULL, 2, { {-4, -4}, {5, 5} } }, // 23
-    { "8\n4 -3\n3 4\n-3 -4\n-4 3\n4 3\n3 -4\n-4 -3\n-3 4\n", NULL, 8, { {-3, -4}, {3, -4}, {4, -3}, {4, 3}, {3, 4}, {-3, 4}, {-4, 3}, {-4, -3} } }, // 24
-
+    { "-1\n", "bad number of points", 0, { {0, 0} } }, // 5
+    { "3\n1 2\n4 3\n5\n", "bad number of lines", 0, { {0, 0} } }, // 6
+    { "0\n", NULL, 0, { {0, 0} } }, // 7
+    { "1\n2 3\n", NULL, 1, { {2, 3} } }, // 8
+    { "2\n1 1\n4 5\n", NULL, 2, { {1, 1}, {4, 5} } }, // 9
+    { "4\n4 4\n2 3\n4 0\n0 4\n", NULL, 3, { {4, 4}, {4, 0}, {0, 4} } }, // 10
+    { "4\n4 0\n2 1\n0 0\n0 4\n", NULL, 3, { {0, 0}, {0, 4}, {4, 0} } }, // 11
+    { "5\n0 0\n2 3\n4 0\n0 4\n4 4\n", NULL, 4, { {0, 0}, {4, 0}, {0, 4}, {4, 4} } }, //12
+    { "8\n7 0\n4 0\n0 0\n3 0\n2 0\n1 0\n5 0\n6 0\n", NULL, 2, { {0, 0}, { 7, 0 } } }, // 13
+    { "8\n0 7\n0 4\n0 0\n0 3\n0 2\n0 1\n0 5\n0 6\n", NULL, 2, { {0, 0}, {0, 7} } }, // 14
+    { "8\n3 3\n-1 -1\n5 5\n-4 -4\n1 1\n2 2\n-2 -2\n3 3\n", NULL, 2, { {-4, -4}, {5, 5} } }, // 15
+    { "8\n2 4\n-2 4\n0 0\n-3 9\n1 1\n-1 1\n3 9\n0 -6\n", NULL, 3, { {3, 9}, {-3, 9}, {0, -6} } }, // 16
+    { "8\n1 -1\n-1 -1\n2 -4\n-3 -9\n-2 -4\n0 0\n3 -9\n0 6\n", NULL, 3, { {3, -9}, {-3, -9}, {0, 6} } }, // 17
+    { "6\n-4 -1\n2 2\n1 4\n-2 -2\n-1 -4\n4 1\n", NULL, 4, { {4, 1}, {1, 4}, {-4, -1}, {-1, -4} } }, // 18
+    { "6\n3 3\n1 4\n-1 -4\n-4 -1\n4 1\n-3 -3\n", NULL, 6, { {4, 1}, {1, 4}, {-4, -1}, {-1, -4}, {3, 3}, {-3, -3} } }, // 19
+    { "8\n4 -3\n3 4\n-3 -4\n-4 3\n4 3\n3 -4\n-4 -3\n-3 4\n", NULL, 8, { {-3, -4}, {3, -4}, {4, -3}, {4, 3}, {3, 4}, {-3, 4}, {-4, 3}, {-4, -3} } }, // 20
+    { "4\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n", NULL, 4, { {0, INT_MIN}, {INT_MAX, 0}, {0, INT_MAX}, {INT_MIN, 0} } }, // 21
+    { "5\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n0 0\n", NULL, 4, { {0, INT_MIN}, {INT_MAX, 0}, {0, INT_MAX}, {INT_MIN, 0} } }, // 22
+    { "5\n2147483647 0\n-2147483648 0\n-2147483647 1\n2147483647 2\n-2147483648 2\n", NULL, 4, { {INT_MAX, 0}, {INT_MAX, 2}, {INT_MIN, 2}, {INT_MIN, 0} } }, // 23
+    { "8\n-2147483648 0\n0 2147483647\n0 -2147483648\n2147483647 0\n-2147483648 -2147483648\n-2147483648 2147483647\n2147483647 -2147483648\n2147483647 2147483647\n", NULL, 4, { {INT_MIN, INT_MIN}, {INT_MAX, INT_MIN}, {INT_MAX, INT_MAX}, {INT_MIN, INT_MAX} } }, // 24
     { "3\n0 2147483647\n0 -2147483648\n1 2147483647\n", NULL, 3, { {0, INT_MAX}, {1, INT_MAX}, {0, INT_MIN} } }, // 25
     { "3\n0 2147483647\n0 -2147483648\n-1 2147483647\n", NULL, 3, { {0, INT_MAX}, {-1, INT_MAX}, {0, INT_MIN} } }, // 26
     { "3\n2147483647 0\n-2147483648 0\n2147483647 -1\n", NULL, 3, { {INT_MAX, 0}, {INT_MAX, -1}, {INT_MIN, 0} } }, // 27
     { "3\n2147483647 0\n-2147483648 0\n2147483647 1\n", NULL, 3, { {INT_MAX, 0}, {INT_MAX, 1}, {INT_MIN, 0} } }, // 28
-
     { "4\n2147483647 0\n-2147483648 0\n0 1\n-2147483648 3\n", NULL, 3, { {INT_MAX, 0}, {INT_MIN, 3}, {INT_MIN, 0} } }, // 29
-    { "5\n2147483647 0\n-2147483648 0\n-2147483647 1\n2147483647 2\n-2147483648 2\n", NULL, 4, { {INT_MAX, 0}, {INT_MAX, 2}, {INT_MIN, 2}, {INT_MIN, 0} } }, // 30
-    { "4\n2147483647 2147483647\n2147483647 2147483644\n-2147483648 -2147483648\n2147483646 2147483645\n", NULL, 3, { {INT_MAX, INT_MAX - 3}, {INT_MAX, INT_MAX}, {INT_MIN, INT_MIN} } }, // 31
-    { "4\n2147483647 -2147483648\n-2147483648 2147483647\n-2147483648 2147483644\n-2147483647 2147483645\n", NULL, 3, { {INT_MAX, INT_MIN}, {INT_MIN, INT_MAX - 3}, {INT_MIN, INT_MAX} } }, // 32
+    { "4\n2147483647 2147483647\n2147483647 2147483644\n-2147483648 -2147483648\n2147483646 2147483645\n", NULL, 3, { {INT_MAX, INT_MAX - 3}, {INT_MAX, INT_MAX}, {INT_MIN, INT_MIN} } }, // 30
+    { "4\n2147483647 -2147483648\n-2147483648 2147483647\n-2147483648 2147483644\n-2147483647 2147483645\n", NULL, 3, { {INT_MAX, INT_MIN}, {INT_MIN, INT_MAX - 3}, {INT_MIN, INT_MAX} } }, // 31
 };
 
 const char* ScanPoint(FILE* file, TPoint* point)
@@ -110,26 +103,23 @@ static int CheckFromArray(void)
             printf("wrong output -- %s\n", Fail);
             return 1;
         }
-
-        passed = !HaveGarbageAtTheEnd(out);
     }   
     else if (testInOut[testN].outError != NULL) {
         char error[128] = {0};
-        const char* status = ScanChars(out, sizeof(error), error);
-
-        if(status == Pass && _strnicmp(testInOut[testN].outError, error, strlen(testInOut[testN].outError)) != 0) {
+        const char* status = ScanChars(out, sizeof(testInOut[testN].outError), error);
+        
+        if(status == Pass &&  _strnicmp(testInOut[testN].outError, error, strlen(error)) != 0) {
             status = Fail;
         }
         
         if (status == Fail) {
             fclose(out);
             printf("wrong output -- ");
+            return 1;
         }
-        
-        passed = status == Pass;
     }
-
-
+    
+    passed = !HaveGarbageAtTheEnd(out);
     ++testN;
     fclose(out);
     printf("%s\n", (passed) ? Pass : Fail);
@@ -413,52 +403,41 @@ const TLabTest LabTests[] = {
     {FeedFromArray, CheckFromArray}, // 2
     {FeedFromArray, CheckFromArray}, // 3
     {FeedFromArray, CheckFromArray}, // 4
-    
     {FeedFromArray, CheckFromArray}, // 5
     {FeedFromArray, CheckFromArray}, // 6
     {FeedFromArray, CheckFromArray}, // 7
-    {FeedFromArray, CheckFromArray}, // 8
-    
+    {FeedFromArray, CheckFromArray}, // 8  
     {FeedFromArray, CheckFromArray}, // 9
     {FeedFromArray, CheckFromArray}, // 10
     {FeedFromArray, CheckFromArray}, // 11
-    {FeedFromArray, CheckFromArray}, // 12
-    
+    {FeedFromArray, CheckFromArray}, // 12    
     {FeedFromArray, CheckFromArray}, // 13
     {FeedFromArray, CheckFromArray}, // 14
     {FeedFromArray, CheckFromArray}, // 15
-    {FeedFromArray, CheckFromArray}, // 16
-    
+    {FeedFromArray, CheckFromArray}, // 16    
     {FeedFromArray, CheckFromArray}, // 17
     {FeedFromArray, CheckFromArray}, // 18
     {FeedFromArray, CheckFromArray}, // 19
-    {FeedFromArray, CheckFromArray}, // 20
-    
+    {FeedFromArray, CheckFromArray}, // 20   
     {FeedFromArray, CheckFromArray}, // 21
     {FeedFromArray, CheckFromArray}, // 22
     {FeedFromArray, CheckFromArray}, // 23
     {FeedFromArray, CheckFromArray}, // 24
-
     {FeedFromArray, CheckFromArray}, // 25
     {FeedFromArray, CheckFromArray}, // 26
     {FeedFromArray, CheckFromArray}, // 27
     {FeedFromArray, CheckFromArray}, // 28
-
     {FeedFromArray, CheckFromArray}, // 29
     {FeedFromArray, CheckFromArray}, // 30
     {FeedFromArray, CheckFromArray}, // 31
-    {FeedFromArray, CheckFromArray}, // 32
-    
+    {FeederBigSegment, CheckerBigSegment}, // 32
     {FeederBigSegment, CheckerBigSegment}, // 33
     {FeederBigSegment, CheckerBigSegment}, // 34
-    {FeederBigSegment, CheckerBigSegment}, // 35
-
+    {FeederBigTriangle, CheckerBigTriangle}, // 35
     {FeederBigTriangle, CheckerBigTriangle}, // 36
-    {FeederBigTriangle, CheckerBigTriangle}, // 37
-
+    {FeederBigParabol, CheckerBigParabol}, // 37
     {FeederBigParabol, CheckerBigParabol}, // 38
-    {FeederBigParabol, CheckerBigParabol}, // 39
-    {FeederBigParabol, CheckerBigParabol}  // 40
+    {FeederBigParabol, CheckerBigParabol}  // 39
 };
 
 TLabTest GetLabTest(int testIdx) {
